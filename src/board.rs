3e7c79777d3a86a65cc1a84f2c5435b04468c7fc
Francis Gulotta <wizard@roborooter.com>
2020-02-19 15:47:09 -0500

    Rustfmt 🤷‍♂️

--- src/board.rs
+++ src/board.rs
@@ -2,140 +2,154 @@ use std::fmt;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Player {
-  X,
-  O,
-  None,
+    X,
+    O,
+    None,
 }
 
 impl fmt::Display for Player {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let output = match self {
-      Player::X => "X",
-      Player::O => "O",
-      Player::None => " ",
-    };
-    write!(f, "{}", output)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let output = match self {
+            Player::X => "X",
+            Player::O => "O",
+            Player::None => " ",
+        };
+        write!(f, "{}", output)
+    }
 }
-
 
 #[derive(Debug)]
 pub struct Board {
-  pub move_count: i32,
-  pub next_player: Player,
-  pub cells: [Player; 9],
+    pub move_count: i32,
+    pub next_player: Player,
+    pub cells: [Player; 9],
 }
 
 #[derive(Debug, PartialEq)]
 pub enum GameState {
-  Won(Player),
-  Playing,
+    Won(Player),
+    Playing,
 }
 
 impl Board {
-
-  pub fn new(next_player: Player) -> Board {
-    if next_player == Player::None {
-      panic!("None is not a valid starting player")
-    }
-    Board {
-      move_count: 0,
-      next_player,
-      cells: [Player::None, Player::None, Player::None, Player::None, Player::None, Player::None, Player::None, Player::None, Player::None]
-    }
-  }
-
-  pub fn play(&mut self, cell: usize) -> Result<(), String> {
-    if self.cells[cell] != Player::None {
-      return Err(format!("Cell {} is already in use by {:?}", cell, self.cells[cell]));
-    }
-    let next_player = match self.next_player {
-      Player::X => Player::O,
-      Player::O => Player::X,
-      Player::None => {
-        return Err(String::from("Board has an invalid next player"));
-      }
-    };
-
-    self.move_count+= 1;
-    self.cells[cell] = self.next_player;
-    self.next_player = next_player;
-    Ok(())
-  }
-
-  pub fn calculate_game_state(&self) -> GameState {
-    let winning_states = [
-      (0,1,2),
-      (3,4,5),
-      (6,7,8),
-      (0,3,6),
-      (1,4,7),
-      (3,5,8),
-      (0,4,8),
-      (2,4,6),
-    ];
-
-    if self.move_count < 5 {
-      return GameState::Playing
-    };
-
-    let cells = self.cells;
-
-    for win_state in winning_states.iter() {
-      for player in [Player::X, Player::O].iter() {
-        if cells[win_state.0] == *player && cells[win_state.1] == *player && cells[win_state.2] == *player {
-          return GameState::Won(*player);
+    pub fn new(next_player: Player) -> Board {
+        if next_player == Player::None {
+            panic!("None is not a valid starting player")
         }
-      }
+        Board {
+            move_count: 0,
+            next_player,
+            cells: [
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+                Player::None,
+            ],
+        }
     }
-    if self.move_count == 9 {
-      GameState::Won(Player::None)
-    } else {
-      GameState::Playing
+
+    pub fn play(&mut self, cell: usize) -> Result<(), String> {
+        if self.cells[cell] != Player::None {
+            return Err(format!(
+                "Cell {} is already in use by {:?}",
+                cell, self.cells[cell]
+            ));
+        }
+        let next_player = match self.next_player {
+            Player::X => Player::O,
+            Player::O => Player::X,
+            Player::None => {
+                return Err(String::from("Board has an invalid next player"));
+            }
+        };
+
+        self.move_count += 1;
+        self.cells[cell] = self.next_player;
+        self.next_player = next_player;
+        Ok(())
     }
-  }
+
+    pub fn calculate_game_state(&self) -> GameState {
+        let winning_states = [
+            (0, 1, 2),
+            (3, 4, 5),
+            (6, 7, 8),
+            (0, 3, 6),
+            (1, 4, 7),
+            (3, 5, 8),
+            (0, 4, 8),
+            (2, 4, 6),
+        ];
+
+        if self.move_count < 5 {
+            return GameState::Playing;
+        };
+
+        let cells = self.cells;
+        for win_state in winning_states.iter() {
+            for player in [Player::X, Player::O].iter() {
+                if cells[win_state.0] == *player
+                    && cells[win_state.1] == *player
+                    && cells[win_state.2] == *player
+                {
+                    return GameState::Won(*player);
+                }
+            }
+        }
+
+        match self.move_count {
+            9 => GameState::Won(Player::None),
+            _ => GameState::Playing,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn new_works() {
-    Board::new(Player::X);
-  }
+    #[test]
+    fn new_works() {
+        Board::new(Player::X);
+    }
 
-  #[test]
-  #[should_panic]
-  fn new_none() {
-    Board::new(Player::None);
-  }
+    #[test]
+    #[should_panic]
+    fn new_none() {
+        Board::new(Player::None);
+    }
 
-  #[test]
-  fn play_works() {
-    let mut board = Board::new(Player::X);
-    board.play(0).unwrap();
-    assert_eq!(board.cells[0], Player::X);
-    board.play(1).unwrap();
-    assert_eq!(board.cells[1], Player::O);
-  }
+    #[test]
+    fn play_works() {
+        let mut board = Board::new(Player::X);
+        board.play(0).unwrap();
+        assert_eq!(board.cells[0], Player::X);
+        board.play(1).unwrap();
+        assert_eq!(board.cells[1], Player::O);
+    }
 
+    #[test]
+    fn cells_cannot_be_reused() {
+        let mut board = Board::new(Player::X);
+        board.play(0).unwrap();
+        board
+            .play(0)
+            .expect_err("cell 0 should not have been allowed to be reused");
+    }
 
-  #[test]
-  fn cells_cannot_be_reused() {
-    let mut board = Board::new(Player::X);
-    board.play(0).unwrap();
-    board.play(0).expect_err("cell 0 should not have been allowed to be reused");
-  }
-
-  #[test]
-  fn calculate_game_state_x_win() {
-    let mut board = Board::new(Player::X);
-    board.play(0).unwrap();
-    board.play(1).unwrap();
-    board.play(3).unwrap();
-    board.play(4).unwrap();
-    board.play(6).unwrap();
-    assert_eq!(board.calculate_game_state(), GameState::Won(Player::X));
-  }
+    #[test]
+    fn calculate_game_state_x_win() {
+        let mut board = Board::new(Player::X);
+        board.play(0).unwrap();
+        board.play(1).unwrap();
+        board.play(3).unwrap();
+        board.play(4).unwrap();
+        board.play(6).unwrap();
+        assert_eq!(board.calculate_game_state(), GameState::Won(Player::X));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 mod board;
-use board::{Board, Player, GameState};
+use board::{Board, GameState, Player};
 
-use std::io::{Write, stdout, stdin};
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     let stdin = stdin();
@@ -18,70 +18,103 @@ fn main() {
     print_board(&mut stdout, &board, cursor);
     for c in stdin.keys() {
         match c.unwrap() {
-          Key::Char('q') => break,
-          Key::Ctrl('c') => break,
-          Key::Esc => break,
-          Key::Left => if cursor != 0 { cursor -= 1 },
-          Key::Right => if cursor < 8 { cursor += 1 },
-          Key::Up => if cursor >= 3 { cursor -= 3 } ,
-          Key::Down => if cursor < 6 { cursor += 3 },
-          Key::Char(' ') => { match board.play(cursor) { Ok(_) => { }, Err(_) => { } }; },
-          Key::Char('\n') => { match board.play(cursor) { Ok(_) => { }, Err(_) => { } }; },
-          _ => { }
+            Key::Char('q') | Key::Ctrl('c') | Key::Esc => break,
+            Key::Left => {
+                if cursor != 0 {
+                    cursor -= 1
+                }
+            }
+            Key::Right => {
+                if cursor < 8 {
+                    cursor += 1
+                }
+            }
+            Key::Up => {
+                if cursor >= 3 {
+                    cursor -= 3
+                }
+            }
+            Key::Down => {
+                if cursor < 6 {
+                    cursor += 3
+                }
+            }
+            Key::Char(' ') | Key::Char('\n') => {
+                match board.play(cursor) {
+                    Ok(_) => {}
+                    Err(_) => {}
+                };
+            }
+            _ => {}
         };
         print_board(&mut stdout, &board, cursor);
         stdout.flush().unwrap();
         match board.calculate_game_state() {
-          GameState::Won(winner) => {
-            print_won(&board, winner);
-            break;
-          },
-          GameState::Playing => {}
+            GameState::Won(winner) => {
+                print_won(&board, winner);
+                break;
+            }
+            GameState::Playing => {}
         }
     }
 }
 
 fn print_won(board: &Board, winner: Player) {
-  print!("\n");
-  match winner {
-    Player::None => println!("Tie!"),
-    _ => println!("{} won in {} moves!", winner, board.move_count)
-  }
+    print!("\n");
+    match winner {
+        Player::None => println!("Tie!"),
+        _ => println!("{} won in {} moves!", winner, board.move_count),
+    }
 }
 
-fn print_board(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>, board: &Board, cursor: usize) {
-  write!(stdout,
-    "{}{}q to exit, arrow keys to move, enter to play\n\n\r",
-    termion::clear::All,
-    termion::cursor::Goto(1, 1),
-  ).unwrap();
-  stdout.flush().unwrap();
+fn print_board(
+    stdout: &mut termion::raw::RawTerminal<std::io::Stdout>,
+    board: &Board,
+    cursor: usize,
+) {
+    write!(
+        stdout,
+        "{}{}q to exit, arrow keys to move, enter to play\n\n\r",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+    )
+    .unwrap();
+    stdout.flush().unwrap();
 
-  print!(
-    "{} | {} | {}\n\r---------\n\r{} | {} | {}\n\r---------\n\r{} | {} | {}\n\r",
-    display_cell(board, cursor, 0),
-    display_cell(board, cursor, 1),
-    display_cell(board, cursor, 2),
-    display_cell(board, cursor, 3),
-    display_cell(board, cursor, 4),
-    display_cell(board, cursor, 5),
-    display_cell(board, cursor, 6),
-    display_cell(board, cursor, 7),
-    display_cell(board, cursor, 8),
-  );
+    print!(
+        "{} | {} | {}\n\r---------\n\r{} | {} | {}\n\r---------\n\r{} | {} | {}\n\r",
+        display_cell(board, cursor, 0),
+        display_cell(board, cursor, 1),
+        display_cell(board, cursor, 2),
+        display_cell(board, cursor, 3),
+        display_cell(board, cursor, 4),
+        display_cell(board, cursor, 5),
+        display_cell(board, cursor, 6),
+        display_cell(board, cursor, 7),
+        display_cell(board, cursor, 8),
+    );
 }
 
 fn display_cell(board: &Board, cursor: usize, cell: usize) -> String {
-  let cells = board.cells;
-  if cursor == cell {
-    let dsp_cell = if cells[cell] == Player::None { board.next_player } else { cells[cell] };
-    return format!("{}{}{}", termion::style::Underline, dsp_cell, termion::style::Reset)
-  }
-  format!("{}",cells[cell])
+    let cells = board.cells;
+    if cursor == cell {
+        let dsp_cell = if cells[cell] == Player::None {
+            board.next_player
+        } else {
+            cells[cell]
+        };
+        return format!(
+            "{}{}{}",
+            termion::style::Underline,
+            dsp_cell,
+            termion::style::Reset
+        );
+    }
+    format!("{}", cells[cell])
 }
 
 fn read_string() -> String {
-  let mut guess = String::new();
-  stdin().read_line(&mut guess).unwrap();
-  guess.trim().to_string()
+    let mut guess = String::new();
+    stdin().read_line(&mut guess).unwrap();
+    guess.trim().to_string()
 }
